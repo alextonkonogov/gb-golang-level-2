4870c733346acd5037b603780b83bc82bb573e66
Alexander Tonkonogov <a.tonkonogov@yadro.com>
2021-10-04 12:19:57 +0300

    homework 2

--- homework2/tasks1and2/main.go
+++ homework2/tasks1and2/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	"github.com/alextonkonogov/gb-golang-level-2/homework2/tasks1and2/myErrors"
+	"log"
 )
 
+//task 1
 //Выполните сборку ваших предыдущих программ под операционную систему, отличающуюся от текущей.
 //Проанализируйте вывод команды file для полученного исполняемого файла.
 //Попробуйте запустить исполняемый файл
+
+//task 2
+//Напишите документацию для одной из предыдущих программ. Запустите сервер документации локально.
+//Убедитесь, что документация отображается корректно.
+
+//На всякий приложил скрины: https://drive.google.com/drive/folders/1lf9J5WV_BkXz9uNHhIY8u0Hat_VehnRV?usp=sharing
 func main() {
 	err := myFuncWithRecoveredPanic()
 	if err != nil {
--- homework2/tasks1and2/myErrors/myErrors_test.go
+++ homework2/tasks1and2/myErrors/myErrors_test.go
@@ -7,7 +7,6 @@ import (
 func Example() {
 	err := New("Alarm! An error has occured!")
 	fmt.Println(err.Error())
-
 	// Output:
 	//Error text: Alarm! An error has occured! | Error time: 2021-10-04 11:14:31.1272 +0300 MSK m=+0.001379126
 }
