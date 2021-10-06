d802d31c6a32074ff1003afc38dc9687a8bc7b8d
Alexander Tonkonogov <a.tonkonogov@yadro.com>
2021-10-06 20:17:35 +0300

    homework 4

--- homework4/task1/main.go
+++ homework4/task1/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+//С помощью пула воркеров написать программу, которая запускает 1000 горутин, каждая из которых увеличивает число на 1.
+//Дождаться завершения всех горутин и убедиться, что при каждом запуске программы итоговое число равно 1000.
+func main() {
+	var counter int
+	n := 1000
+	ch := make(chan int, n)
+	wg := sync.WaitGroup{}
+
+	wg.Add(n)
+	go func(ch <-chan int) {
+		for v := range ch {
+			counter += v
+		}
+		wg.Done()
+	}(ch)
+
+	for i := 0; i < n; i++ {
+		go func(ch chan<- int) {
+			ch <- 1
+			wg.Done()
+		}(ch)
+	}
+
+	wg.Wait()
+	fmt.Printf("The counter is: %v\n", counter)
+}
