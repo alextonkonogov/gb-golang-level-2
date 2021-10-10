b86cba9e4aa80055fad16d0d91024a80d5d9272b
Alexander Tonkonogov <a.tonkonogov@yadro.com>
2021-10-10 23:21:47 +0300

    homework 4 fix races

--- homework4/task1/main.go
+++ homework4/task1/main.go
@@ -8,26 +8,27 @@ import (
 //С помощью пула воркеров написать программу, которая запускает 1000 горутин, каждая из которых увеличивает число на 1.
 //Дождаться завершения всех горутин и убедиться, что при каждом запуске программы итоговое число равно 1000.
 func main() {
-	var counter int
-	n := 1000
-	ch := make(chan int, n)
+	var counter = struct {
+		sync.Mutex
+		n int
+	}{}
+
+	workers := 1000
+
+	ch := make(chan int, workers)
+	defer close(ch)
+
 	wg := sync.WaitGroup{}
-
-	wg.Add(n)
-	go func(ch <-chan int) {
-		for v := range ch {
-			counter += v
-		}
-		wg.Done()
-	}(ch)
-
-	for i := 0; i < n; i++ {
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
 		go func(ch chan<- int) {
-			ch <- 1
+			counter.Lock()
+			defer counter.Unlock()
+			counter.n += 1
 			wg.Done()
 		}(ch)
 	}
 
 	wg.Wait()
-	fmt.Printf("The counter is: %v\n", counter)
+	fmt.Printf("The counter is: %v\n", counter.n)
 }
