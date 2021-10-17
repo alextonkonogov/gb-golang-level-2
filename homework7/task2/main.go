2eabed2483533ab668bca624a6bbc3cc4ce868b0
Alexander Tonkonogov <a.tonkonogov@yadro.com>
2021-10-18 02:50:47 +0300

    homework 7

--- homework7/task2/main.go
+++ homework7/task2/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"log"
+	"reflect"
 )
 
 //Написать функцию, которая принимает на вход имя файла и название функции.
@@ -36,12 +36,7 @@ func CountAsyncFunctions(fileName, funcName string) (count int, err error) {
 				continue
 			}
 			functionExists = true
-			for _, stmt := range fn.Body.List {
-				switch stmt.(type) {
-				case *ast.GoStmt:
-					count++
-				}
-			}
+			count = countGoStmt(fn.Body.List)
 			break
 		}
 	}
@@ -49,4 +44,26 @@ func CountAsyncFunctions(fileName, funcName string) (count int, err error) {
 		err = fmt.Errorf("function with name \"%s\" was not found", funcName)
 	}
 	return
+}
+
+func countGoStmt(stmts []ast.Stmt) int {
+	var count int
+	for _, stmt := range stmts {
+		reflect.ValueOf(stmt)
+		switch v := stmt.(type) {
+		case *ast.GoStmt:
+			count++
+		case *ast.IfStmt:
+			count += countGoStmt(v.Body.List)
+		case *ast.ForStmt:
+			count += countGoStmt(v.Body.List)
+		case *ast.RangeStmt:
+			count += countGoStmt(v.Body.List)
+		case *ast.SwitchStmt:
+			count += countGoStmt(v.Body.List)
+		case *ast.CaseClause:
+			count += countGoStmt(v.Body)
+		}
+	}
+	return count
 }
--- homework7/task2/someGoFileWithCode.go
+++ homework7/task2/someGoFileWithCode.go
@@ -16,5 +16,15 @@ func TheFunctionWithSomeAsyncFunctionsInside() {
 	go log.Println("One")
 	go fmt.Println("Two")
 	go log.Printf("%s", "Three")
+	if true {
+		go log.Printf("%s", "Four")
+	}
+	for i := 0; i < 3; i++ {
+		go log.Println("Five")
+	}
+	switch true {
+	case true:
+		go log.Println("Six")
+	}
 	time.Sleep(5 * time.Second)
 }
